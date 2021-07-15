ab077d0b3f5ecf93baaf462067fefb11ac25a7df
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-15 19:33:07 -0300

    test: ensure AddPropertyController returns 400 if no Publication date is provider

---
+++ backend/src/presentation/controllers/property/add-property.spec.ts
@@ -0,0 +1,25 @@
+import AddPropertyController from './add-property';
+
+describe('Add Imovel', () => {
+  test('Should return 400 if no Publication date is provided', () => {
+    const sut = new AddPropertyController();
+    const httpRequest = {
+      body: {
+        title: 'any_title',
+        description: 'any_description',
+        value: 'any_value',
+        area: 'any_area',
+        address: 'any_address',
+        public_place: 'any_public_place',
+        number: 'any_number',
+        adjunct: 'any_adjunct',
+        neighborhood: 'any_neighborhood',
+        zip_code: 'any_zip_code',
+        city: 'any_city',
+        state: 'any_state',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
