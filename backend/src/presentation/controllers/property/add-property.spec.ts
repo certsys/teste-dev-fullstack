b5f07485b9c19d73215cb1fa8912655de1056e66
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-15 20:31:16 -0300

    test: ensure AddPropertyController returns 400 if no adjunct is provider

--- backend/src/presentation/controllers/property/add-property.spec.ts
+++ backend/src/presentation/controllers/property/add-property.spec.ts
@@ -187,4 +187,27 @@ describe('Add Imovel', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('number'));
   });
+
+  test('Should return 400 if no adjunct is provided', () => {
+    const sut = new AddPropertyController();
+    const httpRequest = {
+      body: {
+        publication_date: 'any_publication_date',
+        title: 'any_title',
+        description: 'any_description',
+        value: 'any_value',
+        area: 'any_area',
+        address: 'any_address',
+        public_place: 'any_public_place',
+        number: 'any_number',
+        neighborhood: 'any_neighborhood',
+        zip_code: 'any_zip_code',
+        city: 'any_city',
+        state: 'any_state',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('adjunct'));
+  });
 });
