9c47238a2d9295ddb3a7524cadca3e21720a1d50
Tiago Cabral <tiagofcabralz@gmail.com>
2021-07-15 18:42:24 -0300

    fix(config): remove unused variable

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,5 @@
 module.exports = (process.env.NODE_ENV === "production") ? {
-  BASE_URL: "",
   BASE_API_URL: "",
 } : {
-  BASE_URL: "http://localhost:8080/",
   BASE_API_URL: "http://localhost:5118/",
 }
