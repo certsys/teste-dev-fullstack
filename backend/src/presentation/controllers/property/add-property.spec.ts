02439ae403858dfc0e596011bcbd4999d56da725
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-15 20:32:18 -0300

    test: ensure AddPropertyController returns 400 if no neighborhood is provider

--- backend/src/presentation/controllers/property/add-property.spec.ts
+++ backend/src/presentation/controllers/property/add-property.spec.ts
@@ -210,4 +210,27 @@ describe('Add Imovel', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('adjunct'));
   });
+
+  test('Should return 400 if no neighborhood is provided', () => {
+    const sut = new AddPropertyController();
+    const httpRequest = {
+      body: {
+        publication_date: 'any_publication_date',
+        title: 'any_title',
+        description: 'any_description',
+        value: 'any_value',
+        area: 'any_area',
+        address: 'any_address',
+        public_place: 'any_public_place',
+        number: 'any_number',
+        adjunct: 'any_adjunct',
+        zip_code: 'any_zip_code',
+        city: 'any_city',
+        state: 'any_state',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('neighborhood'));
+  });
 });
