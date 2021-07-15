099fc25c4c5311592df35b206ecbc415e349a64f
Tiago Cabral <tiagofcabralz@gmail.com>
2021-07-15 18:41:30 -0300

    feat(swagger): add api documentation

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
 
 //Import Routes
 const User = require('./routes/UserRoute'); 
@@ -10,6 +12,7 @@ const Property = require('./routes/PropertyRoute');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 //Routes
 app.use('/User', User);
