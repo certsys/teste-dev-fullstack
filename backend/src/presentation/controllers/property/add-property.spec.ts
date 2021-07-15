78ef8df5f491ee89967e8248d4a27c0a8215b818
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-15 20:29:10 -0300

    test: ensure AddPropertyController returns 400 if no public_place is provider

--- backend/src/presentation/controllers/property/add-property.spec.ts
+++ backend/src/presentation/controllers/property/add-property.spec.ts
@@ -141,4 +141,27 @@ describe('Add Imovel', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('address'));
   });
+
+  test('Should return 400 if no public_place is provided', () => {
+    const sut = new AddPropertyController();
+    const httpRequest = {
+      body: {
+        publication_date: 'any_publication_date',
+        title: 'any_title',
+        description: 'any_description',
+        value: 'any_value',
+        area: 'any_area',
+        address: 'any_address',
+        number: 'any_number',
+        adjunct: 'any_adjunct',
+        neighborhood: 'any_neighborhood',
+        zip_code: 'any_zip_code',
+        city: 'any_city',
+        state: 'any_state',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('public_place'));
+  });
 });
