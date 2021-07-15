b618d4b29685199baea2b4745a9e1b21c962c2fa
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-15 20:39:15 -0300

    refactor: make AddPropertyController conforms to a protocol

--- backend/src/presentation/controllers/property/add-property.ts
+++ backend/src/presentation/controllers/property/add-property.ts
@@ -1,8 +1,9 @@
 import { MissingParamError } from '../../errors/missing-param-error';
 import { badRequest } from '../../helpers/http-helper';
+import { Controller } from '../../protocols/controller';
 import { HttpRequest, HttpResponse } from '../../protocols/http';
 
-export default class AddPropertyController {
+export default class AddPropertyController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = [
       'publication_date',
