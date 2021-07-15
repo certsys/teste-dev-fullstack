11fbf954582bbb1a66790f9f9554ead15f3f2377
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-15 20:57:43 -0300

    refactor: move AddPropertyController to a folder

--- backend/src/presentation/controllers/property/add-property/add-property.spec.ts
+++ backend/src/presentation/controllers/property/add-property/add-property.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from '../../errors/missing-param-error';
+import { MissingParamError } from '../../../errors/missing-param-error';
 import AddPropertyController from './add-property';
 
 describe('Add Imovel', () => {
