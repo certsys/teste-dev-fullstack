46edbf1d651f891e349edf3295652e807ec51f1a
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-15 20:27:41 -0300

    test: ensure AddPropertyController returns 400 if no address is provider

--- backend/src/presentation/controllers/property/add-property.spec.ts
+++ backend/src/presentation/controllers/property/add-property.spec.ts
@@ -118,4 +118,27 @@ describe('Add Imovel', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('area'));
   });
+
+  test('Should return 400 if no address is provided', () => {
+    const sut = new AddPropertyController();
+    const httpRequest = {
+      body: {
+        publication_date: 'any_publication_date',
+        title: 'any_title',
+        description: 'any_description',
+        value: 'any_value',
+        area: 'any_area',
+        public_place: 'any_public_place',
+        number: 'any_number',
+        adjunct: 'any_adjunct',
+        neighborhood: 'any_neighborhood',
+        zip_code: 'any_zip_code',
+        city: 'any_city',
+        state: 'any_state',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('address'));
+  });
 });
