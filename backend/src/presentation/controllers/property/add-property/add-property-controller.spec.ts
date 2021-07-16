b0d79011989282e0b182792ddd1f19fa0a67ea19
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-15 23:04:20 -0300

    test: ensure AddPropertyController returns a badRequest error if validation fails

--- backend/src/presentation/controllers/property/add-property/add-property-controller.spec.ts
+++ backend/src/presentation/controllers/property/add-property/add-property-controller.spec.ts
@@ -1,6 +1,7 @@
 import { HttpRequest, Validation } from './add-property-controller-protocols';
 
 import AddPropertyController from './add-property-controller';
+import { badRequest } from '../../../helpers/http-helper';
 
 const makeFakeRequest = (): HttpRequest => {
   return {
@@ -53,5 +54,12 @@ describe('AddProperty Controller', () => {
     const httpRequest = makeFakeRequest();
     await sut.handle(httpRequest);
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  test('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut();
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error());
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(badRequest(new Error()));
   });
 });
