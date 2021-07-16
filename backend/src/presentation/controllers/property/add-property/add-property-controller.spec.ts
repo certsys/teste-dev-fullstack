35aeacb305777c08904f1616f42f227d5650339f
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-15 22:57:40 -0300

    refactor: move sut creation to a factory method

--- backend/src/presentation/controllers/property/add-property/add-property-controller.spec.ts
+++ backend/src/presentation/controllers/property/add-property/add-property-controller.spec.ts
@@ -22,16 +22,34 @@ const makeFakeRequest = (): HttpRequest => {
   };
 };
 
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error {
+      return null;
+    }
+  }
+
+  return new ValidationStub();
+};
+
+interface SutTypes {
+  sut: AddPropertyController;
+  validationStub: Validation;
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation();
+  const sut = new AddPropertyController(validationStub);
+  return {
+    sut,
+    validationStub,
+  };
+};
+
 describe('AddProperty Controller', () => {
   test('Should call Validation with correct values', async () => {
-    class ValidationStub implements Validation {
-      validate(input: any): Error {
-        return null;
-      }
-    }
-    const validationStub = new ValidationStub();
+    const { sut, validationStub } = makeSut();
     const validateSpy = jest.spyOn(validationStub, 'validate');
-    const sut = new AddPropertyController(validationStub);
     const httpRequest = makeFakeRequest();
     await sut.handle(httpRequest);
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
