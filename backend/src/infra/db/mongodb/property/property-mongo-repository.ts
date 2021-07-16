62fafc6990889922e34a57ac0f26d5f952633344
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-16 01:53:43 -0300

    feat: ensure PropertyMongoRepository add a new property on add success

---
+++ backend/src/infra/db/mongodb/property/property-mongo-repository.ts
@@ -0,0 +1,10 @@
+import { AddPropertyRepository } from '../../../../data/protocols/db/property/add-property-repository';
+import { AddPropertyModel } from '../../../../domain/usecases/add-property';
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class PropertyMongoRepository implements AddPropertyRepository {
+  async add(propertyData: AddPropertyModel): Promise<void> {
+    const propertyCollection = await MongoHelper.getCollection('propertys');
+    await propertyCollection.insertOne(propertyData);
+  }
+}
