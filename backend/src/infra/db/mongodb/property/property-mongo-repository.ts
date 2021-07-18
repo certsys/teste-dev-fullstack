0d00f606c40d3536bed67fd3bbea279040d6485b
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-18 18:03:38 -0300

    feat: ensure PropertyMongoRepositore update the property finded by id on success

--- backend/src/infra/db/mongodb/property/property-mongo-repository.ts
+++ backend/src/infra/db/mongodb/property/property-mongo-repository.ts
@@ -2,7 +2,8 @@ import { ObjectId } from 'mongodb';
 import { AddPropertyRepository } from '../../../../dataLayer/protocols/db/property/add-property-repository';
 import { LoadPropertiesRepository } from '../../../../dataLayer/protocols/db/property/load-properties-repository';
 import { LoadPropertyRepository } from '../../../../dataLayer/protocols/db/property/load-property-repository';
-import { PropertyModel } from '../../../../domain/models/property';
+import { UpdatePropertyRepository } from '../../../../dataLayer/protocols/db/property/update-property-repository';
+import { PropertyModel, UpdatePropertyModel } from '../../../../domain/models/property';
 import { AddPropertyModel } from '../../../../domain/usecases/add-property';
 import { MongoHelper } from '../helpers/mongo-helper';
 
@@ -10,7 +11,8 @@ export class PropertyMongoRepository
   implements
     AddPropertyRepository,
     LoadPropertiesRepository,
-    LoadPropertyRepository
+    LoadPropertyRepository,
+    UpdatePropertyRepository
 {
   async add(propertyData: AddPropertyModel): Promise<void> {
     const propertyCollection = await MongoHelper.getCollection('properties');
@@ -30,5 +32,19 @@ export class PropertyMongoRepository
       _id: newId,
     });
     return property;
+  }
+
+  async update(updateData: UpdatePropertyModel): Promise<any | null> {
+    const propertyCollection = await MongoHelper.getCollection('properties');
+
+    const newId = new ObjectId(updateData.id);
+    // console.log(newId);
+    const body = updateData.body;
+    const property = await propertyCollection.findOneAndUpdate(
+      { _id: newId },
+      { $set: { ...body } },
+      { returnOriginal: false },
+    );
+    return property.value;
   }
 }
