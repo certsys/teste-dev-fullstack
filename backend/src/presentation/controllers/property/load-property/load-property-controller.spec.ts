1c901c9e2df8431f1264123d42ec3177f8ac8e9f
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-18 01:08:03 -0300

    test: ensure LoadPropertyController returns 500 if LoadProperty throws

--- backend/src/presentation/controllers/property/load-property/load-property-controller.spec.ts
+++ backend/src/presentation/controllers/property/load-property/load-property-controller.spec.ts
@@ -4,7 +4,12 @@ import {
 } from './load-property-controller-protocols';
 
 import MockDate from 'mockdate';
-import { noContent, ok, serverError } from '../../../helpers/http-helper';
+import {
+  noContent,
+  notFound,
+  ok,
+  serverError,
+} from '../../../helpers/http-helper';
 import { LoadPropertyController } from './load-property-controller';
 
 const makeFakeProperty = (): PropertyModel => {
@@ -60,11 +65,34 @@ describe('LoadProperty Controller', () => {
 
   test('Should return 200 on success', async () => {
     const { sut, loadPropertyStub } = makeSut();
-    const loadSpy = jest.spyOn(loadPropertyStub, 'load');
     const httpRequest = {
       params: 'any_id',
     };
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(ok(makeFakeProperty()));
+  });
+
+  test('Should return 404 if any property is not found', async () => {
+    const { sut, loadPropertyStub } = makeSut();
+    jest
+      .spyOn(loadPropertyStub, 'load')
+      .mockReturnValueOnce(new Promise(resolve => resolve(null)));
+    const httpRequest = {
+      params: 'worng_id',
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(notFound());
+  });
+
+  test('Should return 500 if LoadProperty throws', async () => {
+    const { sut, loadPropertyStub } = makeSut();
+    jest
+      .spyOn(loadPropertyStub, 'load')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+
+    const httpResponse = await sut.handle({});
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
