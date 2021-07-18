26efaf9b55508bd09c5db4ef46c031383f6c22b5
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-18 15:14:12 -0300

    test: ensure UpdatePropertyController returns 400 if hava some value incorrect on body

---
+++ backend/src/presentation/controllers/property/update-property/update-property-controller.spec.ts
@@ -0,0 +1,154 @@
+import {
+  UpdateProperty,
+  PropertyModel,
+  UpdatePropertyModel,
+  HttpRequest,
+} from './update-property-controller-protocols';
+
+import MockDate from 'mockdate';
+import {
+  notFound,
+  ok,
+  serverError,
+  WorngFieldsRequest,
+} from '../../../helpers/http-helper';
+import { UpdatePropertyController } from './update-property-controller';
+import { VerifyFields } from '../../../protocols/verify-fields';
+import { WrongParamError } from '../../../errors/wrong-param-error';
+
+const makeFakeProperty = (): PropertyModel => {
+  return {
+    _id: 'any_id',
+    publication_date: new Date(),
+    title: 'any_title',
+    description: 'any_description',
+    value: 0,
+    area: 'any_area',
+    address: 'any_address',
+    public_place: 'any_public_place',
+    number: 'any_number',
+    adjunct: 'any_adjunct',
+    neighborhood: 'any_neighborhood',
+    zip_code: 'any_zip_code',
+    city: 'any_city',
+    state: 'any_state',
+  };
+};
+
+const makeFakeRequest = (): HttpRequest => {
+  return {
+    params: {
+      id: 'any_id',
+    },
+    body: {
+      publication_date: new Date(),
+      title: 'any_title',
+      description: 'any_description',
+      value: 'any_value',
+      area: 'any_area',
+      address: 'any_address',
+      public_place: 'any_public_place',
+      number: 'any_number',
+      adjunct: 'any_adjunct',
+      neighborhood: 'any_neighborhood',
+      zip_code: 'any_zip_code',
+      city: 'any_city',
+      state: 'any_state',
+    },
+  };
+};
+
+const makeFakeWorngRequest = (): HttpRequest => {
+  return {
+    params: {
+      id: 'any_id',
+    },
+    body: {
+      publication_date: new Date(),
+      title: 'any_title',
+      descriptionnn: 'any_description',
+    },
+  };
+};
+
+interface SutTypes {
+  sut: UpdatePropertyController;
+  updatePropertyStub: UpdateProperty;
+  veryfyFieldsStub: VerifyFields;
+}
+
+const makeUpdateProperty = (): UpdateProperty => {
+  class UpdatePropertyStub implements UpdateProperty {
+    async update(
+      updateData: UpdatePropertyModel,
+    ): Promise<PropertyModel | null> {
+      return new Promise(resolve => resolve(makeFakeProperty()));
+    }
+  }
+  return new UpdatePropertyStub();
+};
+
+const makeVerifyFields = (): VerifyFields => {
+  class VerifyInputFieldStub implements VerifyFields {
+    verify(body: any): string[] {
+      const inputs = [
+        'publication_date',
+        'title',
+        'description',
+        'value',
+        'area',
+        'address',
+        'public_place',
+        'number',
+        'adjunct',
+        'neighborhood',
+        'zip_code',
+        'city',
+        'state',
+      ];
+      const { _id, ...b } = body;
+      const fields = Object.keys({ ...b });
+      const errors = fields.filter(field => {
+        const value = inputs.filter(value => value === field);
+        if (!value.length) {
+          return new WrongParamError(field);
+        }
+      });
+      if (errors.length) {
+        return errors;
+      }
+    }
+  }
+  return new VerifyInputFieldStub();
+};
+
+const makeSut = (): SutTypes => {
+  const veryfyFieldsStub = makeVerifyFields();
+  const updatePropertyStub = makeUpdateProperty();
+  const sut = new UpdatePropertyController(updatePropertyStub);
+  return {
+    sut,
+    updatePropertyStub,
+    veryfyFieldsStub,
+  };
+};
+
+describe('LoadProperty Controller', () => {
+  beforeAll(() => {
+    MockDate.set(new Date());
+  });
+
+  afterAll(() => {
+    MockDate.reset();
+  });
+
+  test('Should return 400 if have values incorrects', async () => {
+    const { sut, veryfyFieldsStub } = makeSut();
+    const veryfyFieldsSpy = jest
+      .spyOn(veryfyFieldsStub, 'verify')
+      .mockReturnValueOnce([`{ statusCode: 400, body: 'descriptionnn' }`]);
+    const httpRequest = makeFakeWorngRequest();
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(WorngFieldsRequest(['descriptionnn']));
+  });
+});
