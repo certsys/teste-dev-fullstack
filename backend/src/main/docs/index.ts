93248b687be5b1460d65094c7e09a025f04b4caa
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-07-20 20:30:10 -0300

    fix: add swagger right local

---
+++ backend/src/main/docs/index.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'CertImóveis API',
+    description: 'Aplicação de venda de imóveis feita em Nodejs e Reactjs com Typescript, TDD, clean arquitecture e Docker no backend, para o processo seletivo de Desenvolvedor Fullstack da Certsys.',
+    version: '1.0.0'
+  }
+}
