18e5e17a2be4177bea736e767048551218d5609b
pedrosouza44 <pedro_souza44@hotmail.com>
2021-07-23 00:07:25 -0300

    Configuração da API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, empresa, Accept, token, enctype ,count,authorization')
+
+  app.use(cors());
+
+  next();
+});
 
 require('./app/controllers/index')(app);
 
