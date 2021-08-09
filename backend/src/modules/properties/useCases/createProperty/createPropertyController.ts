2fd0040076b5553a56288fc570835283b52a10fc
Josimar16 <josimarjr479@gmail.com>
2021-08-09 15:41:23 -0300

    refactor: :recycle: retirando os dados de localização
    por causa da implementação do viacep, os dados serão pegos por ele

--- backend/src/modules/properties/useCases/createProperty/createPropertyController.ts
+++ backend/src/modules/properties/useCases/createProperty/createPropertyController.ts
@@ -3,35 +3,25 @@ import { container } from 'tsyringe';
 import { CreatePropertyUseCase } from './createPropertyUseCase';
 
 class CreatePropertyController {
-  async handle(request: Request, response: Response): Promise<Response> {
+  async handle(request: Request, response: Response): Promise<Response> {   
     const { title,
       description,
       value,
       area,
       address,
-      public_place,
       house_number,
-      complement,
-      district,
-      cep,
-      city,
-      uf } = request.body;
+      cep } = request.body;
 
     const createPropertyUseCase = container.resolve(CreatePropertyUseCase);
-
+      
     const property = await createPropertyUseCase.execute({ 
       title,
       description,
       value,
       area,
       address,
-      public_place,
       house_number,
-      complement,
-      district,
       cep,
-      city,
-      uf
     });
 
     return response.status(201).json(property);
