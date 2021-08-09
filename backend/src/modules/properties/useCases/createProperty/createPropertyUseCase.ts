760077e4992fd871312a7a8bf06a9a2a4e254e1c
Josimar16 <josimarjr479@gmail.com>
2021-08-09 15:42:19 -0300

    refactor: :recycle: Retirando os dados de localização da request
    por causa da implementação do viacep, os dados serão pegos por ele

--- backend/src/modules/properties/useCases/createProperty/createPropertyUseCase.ts
+++ backend/src/modules/properties/useCases/createProperty/createPropertyUseCase.ts
@@ -1,14 +1,19 @@
 import { inject, injectable } from 'tsyringe';
+import { IRequestPropertyDTO } from '@modules/properties/dtos/IRequestPropertyDTO';
 import { ICreatePropertyDTO } from '@modules/properties/dtos/ICreatePropertyDTO';
 import { Property } from '@modules/properties/infra/typeorm/entities/Property';
 import { IPropertiesRepository } from '@modules/properties/repositories/IPropertiesRepository';
 import { AppError } from '@errors/AppError';
+import { ICEPProvider } from '@shared/container/CEPProvider/models/ICEPProvider';
 
 @injectable()
 class CreatePropertyUseCase {
   constructor(
     @inject('PropertiesRepository')
     private propertiesRepository: IPropertiesRepository,
+
+    @inject('CEPProvider')
+    private cepRepository: ICEPProvider,
   ) { }
 
   async execute({
@@ -17,19 +22,22 @@ class CreatePropertyUseCase {
     value,
     area,
     address,
-    public_place,
     house_number,
-    complement,
-    district,
     cep,
-    city,
-    uf,
-  }: ICreatePropertyDTO): Promise<Property> {
+  }: IRequestPropertyDTO): Promise<Property> {
     const propertyAlreadyExist = await this.propertiesRepository.findByTitle(title);
 
     if (propertyAlreadyExist) {
       throw new AppError('Property already exist!');
     }
+
+    const {
+      public_place,
+      district,
+      complement,
+      city,
+      uf
+    } = await this.cepRepository.recoverAddress(cep);
 
     const property = await this.propertiesRepository.create({
       title,
